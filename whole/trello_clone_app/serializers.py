cf9e2859bc18997c8c17ac3dedeed73ffd1d3085
Graham Brown <GrahamDavidBrown90@gmail.com>
2017-04-02 09:13:14 -0400

    got the serializers working again, we changed the order of the foreign keys

--- whole/trello_clone_app/serializers.py
+++ whole/trello_clone_app/serializers.py
@@ -5,22 +5,22 @@ from .models import UserProfile, Board, List, Card
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('id', 'username', 'name_first', 'name_last', 'boards_id', 'user_id')
+        fields = ('id', 'username', 'name_first', 'name_last', 'user_id')
 
 
 class BoardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Board
-        fields = ('id', 'title', 'description', 'lists_id')
+        fields = ('id', 'title', 'description', 'users_id')
 
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = List
-        fields = ('id', 'tite', 'description', 'cards_id')
+        fields = ('id', 'tite', 'description', 'boards_id')
 
 
 class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
-        fields = ('id', 'title', 'description', 'comment')
+        fields = ('id', 'title', 'description', 'comment', 'priority', 'lists_id')
